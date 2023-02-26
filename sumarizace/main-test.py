d514b522144cdfed0357192932dbd9a159da66bf
Adam Kopřiva <Admin.Nuklear@seznam.cz>
2023-02-26 15:04:54 +0100

    working sumarization without prefix

--- sumarizace/main-test.py
+++ sumarizace/main-test.py
@@ -63,7 +63,39 @@ def sumarization():
 	BigSmallIp = min_max(list_of_IPs)
 	print(BigSmallIp)
 
+	max = BigSmallIp[0].split(".")
+	min = BigSmallIp[1].split(".")
 
+	maxBin = []
+	minBin = []
+	sumBin = []
+	sumIp = ""
+
+	for i in range(len(max)):
+		maxBin.append(bin(int(max[i]))[2:].zfill(8))
+		minBin.append(bin(int(min[i]))[2:].zfill(8))
+
+	print(maxBin, minBin)
+
+	for i in range(len(maxBin)):
+		same = True
+		sumSeg = ""
+		for y in range(len(maxBin[i])):
+			if same and maxBin[i][y] == minBin[i][y]:
+				sumSeg = sumSeg + str(maxBin[i][y])
+			else:
+				same = False
+			if same == False:
+				sumSeg = sumSeg + "0"
+		print(sumSeg)
+		sumBin.append(sumSeg)
+	print(sumBin)
+
+	for number in sumBin:
+		sumIp += str(int(number, 2))+"."
+	
+	dpg.set_value("VysledekSumarizace", sumIp[:-1])
+	same = True
 
 def get_full_ip():
 	"""získá celou Ip z inputu, v D i B"""
@@ -97,10 +129,14 @@ def get_full_ip():
 with dpg.window(tag="Primary Window", label="Sumarizace IPv4", width=sirka, height=vyska):
 
 	dpg.add_group(tag="radky")
-	with dpg.group():
+	with dpg.group(horizontal=True):
 		dpg.add_button(label="+", tag="plus_button", callback=lambda: add_radek()) #přidá další řádek na IP
 		dpg.add_button(label="Count", callback=get_full_ip) #spočítá sumarizační ip adresu
 		dpg.add_checkbox(label="Binární", tag="binary") #pokud checked - vypíše je v binárním tvaru
+		
+	with dpg.group(tag="Sumarizovana IP", horizontal=True):
+		dpg.add_text("Sumarizovaná IP adresa: ")
+		dpg.add_text(tag="VysledekSumarizace")
 
 dpg.set_primary_window("Primary Window", True)
 dpg.setup_dearpygui()
